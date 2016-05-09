e4829c1d2e2b2680a655b2878095c841b1c40fc6
Bill Rowe <wer61537@yahoo.com>
2016-05-08 20:17:13 -0500

    Adjust for data since 1996

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,29 @@
+#The primary questions are
+#Does the analysis address the question of which types of events are most harmful to population health?
+#Does the analysis address the question of which types of events have the greatest economic consequences?
+
 #remove all objects just to be safe
 rm(list = ls(all = TRUE))
-
+#=======================================
 #Setting up the environment
+#=======================================
 #get needed libraries
 library(plyr, warn.conflicts = FALSE)
 library(dplyr, warn.conflicts = FALSE)
 library(ggplot2, warn.conflicts = FALSE)
+library(lubridate, warn.conflicts = FALSE)
 library(knitr, warn.conflicts = FALSE)
 library(stringr, warn.conflicts = FALSE)
 library(gridExtra, warn.conflicts = FALSE)
 library(grid, warn.conflicts = FALSE)
 
-
+#=======================================
 # Data Processing
+#=======================================
 # Documentation is at https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2Fpd01016005curr.pdf.
+
 ## Load the data
+
 ### download the file, if necessary
 if (!file.exists("./data/repdata-data-StormData.csv.bz2"))
   download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
@@ -64,18 +73,25 @@ if (!exists("storm.data")){
                         )
   cat("Storm data loaded!")
 } 
+
 ## structure of data
 str(storm.data)
+
+#create a working copy for recovery if needed
+storm.data.orig<-storm.data
+
+#restrict the data to 1996 to current
+
+storm.data$BGN_DATE<-year(mdy_hms(storm.data$BGN_DATE))
+storm.data <- storm.data %>% filter(BGN_DATE >= 1996)
+str(storm.data)
+
 #Tidy the data
 #check to see if the variable names are "tidy"
-if(grep("-",names(storm.data))>0){
+if(sum(grep("-",names(storm.data)))>0){
   names(storm.data) <- gsub("-", "_", names(storm.data)) 
 }
 
-#check for missing data and remove any
-if(sum(is.na(storm.data))>0){
-  storm.data <- storm.data[complete.cases(storm.data), ]
-}
 
 ## convert to upper case, remove multiple spaces and any preceding and trailing spaces
 if (sum(!(grepl("^[[:upper:]]+$", storm.data$EVTYPE)))>0){
@@ -91,22 +107,51 @@ if (sum(!(grepl("^[[:upper:]]+$", storm.data$CROPDMGEXP)))>0){
   storm.data$CROPDMGEXP<-toupper(str_trim(gsub(pattern = "\\s\\s+", replacement = " ", storm.data$CROPDMGEXP),side="both"))
 }
 
-storm.data$EVTYPE[(storm.data$EVTYPE)=="?"]<- "Unknown"
+#CLEAN UP EVENT NAMES
+# Set event types with "Summary" or "Metro" to NA, as these are database errors 
+storm.data$EVTYPE <- gsub("^SUMMARY.*|^METRO.*", NA, storm.data$EVTYPE)
+#recast ?
+storm.data$EVTYPE <- gsub("\\?", "Unknown", storm.data$EVTYPE)
 
+#clean up messy names in events
+storm.data[grep(".*FLOOD|RAIN|PRECIP|FLD|URBAN*.", storm.data$EVTYPE, perl=T),]$EVTYPE <-"FLOOD"
+storm.data[grep(".*DROUGHT|DRY|HOT|HYPERTHERMIA|WARM|TEMPERATURE*.", storm.data$EVTYPE, perl=T),]$EVTYPE <-"DROUGHT"
+storm.data[grep(".*TORNADO|ADO|SPOUT|FUNNEL|STRONG WIND|HIGH WIND|GUST|WHIRL*.", storm.data$EVTYPE, perl=T),]$EVTYPE <-"TORNADO"
+storm.data[grep(".*WINTER|IC[EY]|SNOW|SLEET|BLIZZ|HYPO|THUNDERSNOW|COLD*.", storm.data$EVTYPE, perl=T),]$EVTYPE <-"WINTER STORM"
+storm.data[grep(".*FIRE*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"WILDFIRE"
+storm.data[grep(".*FREEZE|EXTREME COLD|EXTENDED COLD|COLD|CHILL*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"FREEZING"
+storm.data[grep(".*THU[DN]|TSTM*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"THUNDERSTORM"
+storm.data[grep(".*SURGE|TYPHOON|SURF|HURRI|TIDE|COAST|MARINE|SWELLS|SEA|REMNANTS|WAVE|TROPI*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"TROPICAL STORM"
+storm.data[grep(".*HAIL*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"HAIL"
+storm.data[grep(".*HEAT*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"HEAT"
+storm.data[grep(".*SLIDE*.", storm.data$EVTYPE, perl=T),]$EVTYPE<-"LANDSLIDES"
+
+sort(unique(storm.data$EVTYPE))
+
+#check for missing data and remove any
+if(sum(is.na(storm.data))>0){
+  storm.data <- storm.data[complete.cases(storm.data), ]
+}
+#=============================================
 #fatalities and injuries
+#=============================================
 sort(unique(storm.data$FATALITIES))
 sort(unique(storm.data$INJURIES))
+
 #data seems fine
 pers<-aggregate(cbind(storm.data$FATALITIES, storm.data$INJURIES)~storm.data$EVTYPE, data=storm.data, FUN=sum, na.rm=TRUE)
 names(pers) =c("Event","Fatalities","Injuries")
 pers2<-pers[!with(pers,Fatalities==0 & Injuries==0),]
 pers2$Total_Pers_Cost<-pers2$Fatalities + pers2$Injuries
 
-#select top 20
+#select top 10
 pers2<-pers2[ order(-pers2[,4], pers2[,1]), ]
 pers2.top <- pers2[1:10,]
 str(pers2.top)
 pers2.top
+
+#create table of values
+grid.table(pers2.top)
 
 dth <- ggplot(pers2.top, aes(x=Event,y=Fatalities )) + 
   geom_bar(stat="identity") + coord_flip() + 
@@ -118,11 +163,11 @@ inj <- ggplot(pers2.top, aes(x=Event,y=Injuries )) +
   labs(title="Injuries Caused by Weather Events", x = "Event", y="Number of Injuries")
 print(inj)
 
-#plot_grid(dth, inj, labels = c("A","B"))
-
 grid.arrange(dth, inj, nrow=2)
 
+#=============================================
 #economic cost
+#=============================================
 #check DMGEXP columns 
 # The cost of damages is the product of PROPDMG * PROPDMGEXP and CROPDMG * CROPDMGEXP.
 # the DMGEX" columns have the magnitude of the DMG.  So, PROPDMG =25 and PROPDMEXP =K 
@@ -159,40 +204,41 @@ storm.data$CROPDMGEXP <- as.numeric(storm.data$CROPDMGEXP)
 storm.data$PROPCOST <- storm.data$PROPDMG*10^storm.data$PROPDMGEXP
 storm.data$CROPCOST <- storm.data$CROPDMG*10^storm.data$CROPDMGEXP
 
-#clean up variations in names
-sort(unique(storm.data$EVTYPE))
-
 #which events had fatalities, injuries, crop or property damage and crop or property expense
 #calculate mean for each activity, subject combination
 econ<-aggregate(cbind(storm.data$PROPCOST, storm.data$CROPCOST)~storm.data$EVTYPE, data=storm.data, FUN=sum, na.rm=TRUE)
+
+#more friendly names
 names(econ) =c("Event","Property","Crop")
 
+#remove records where porperty and crop damage is zero
 econ2<-econ[!with(econ,Property==0.000000e+00 & Crop==0),]
-names(econ2)
 str(econ2)
 
+#sum to get total economic cost
 econ2$Total_Econ_Cost<-econ2$Property + econ2$Crop
-
+#sort from high to low
 econ2<-econ2[ order(-econ2[,4], econ2[,1]), ][1:10,]
-econ2
-
+#do running total
 econ2 %>% group_by(Event) %>% mutate(cumsum = cumsum(Total_Econ_Cost))
 
+#extract property and crop damage
 prop2<-econ2[ order(-econ2[,2], econ2[,1]), ][1:10,]
 crop2<-econ2[ order(-econ2[,3], econ2[,1]), ][1:10,]
 
-
+#plot total economic cost
 totl <- ggplot(econ2, aes(x=Event,y=Total_Econ_Cost )) + 
   geom_bar(stat="identity") + coord_flip() + 
   labs(title="Total Economic Cost Caused by Weather Events", x = "Event", y="Cost")
 print(totl)
 
-
+#plot property costs
 prop <- ggplot(prop2, aes(x=Event,y=Property )) + 
   geom_bar(stat="identity") + coord_flip() + 
   labs(title="Cost of Property Damage Caused by Weather Events", x = "Event", y="Cost")
 print(prop)
 
+#plot crop costs
 crop <- ggplot(crop2, aes(x=Event,y=Crop )) + 
   geom_bar(stat="identity") + coord_flip() + 
   labs(title="Cost of Crop Damage Caused by Weather Events", x = "Event", y="Cost")
@@ -200,10 +246,8 @@ print(crop)
 
 grid.arrange(totl,prop, crop, nrow=3)
 
+#create table of values
 grid.table(econ2)
 
-#The primary questions are
-#Does the analysis address the question of which types of events are most harmful to population health?
-#Does the analysis address the question of which types of events have the greatest economic consequences?
-
-econ2[, ] <- formatC(as.numeric(econ2[,c('Property','Cost','Total_Econ_Cost') ]), big.mark = ',', digits = 0, format = 'f')
+#write.csv(econ2,"c:/coursera/econ2.csv")
+#write.csv(pers2.top,"c:/coursera/pers2.top.csv")
